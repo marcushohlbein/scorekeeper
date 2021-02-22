3e42e9f687a2dea5309e4fafc8a1bc35427f3ea4
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-22 15:58:52 +0100

    Adjust styling

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import './PlayerForm.css'
 
-function PlayerForm(label, onAddPlayer) {
+function PlayerForm({ label, onAddPlayer }) {
   return (
     <form
+      className="PlayerForm"
       onSubmit={e => {
         e.preventDefault()
         onAddPlayer(e.target.value)
       }}
     >
-      <label>
-        {label}
-        <input type="text" name="playerName" />
-      </label>
+      <label for="playerName">{label}</label>
+      <input type="text" id="playerName" name="playerName" />
     </form>
   )
 }
