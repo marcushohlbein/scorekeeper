feea969888fbb4ecf81eee2e9d0bc9b6d0c90d03
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-22 15:51:14 +0100

    Add player component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import './App.css'
+import Player from './components/Player'
 
 function App() {
   return (
-    
+    <>
+      <Player
+        name="Jone Doe"
+        score="20"
+        onMinus={() => console.log('Minus')}
+        onPlus={() => console.log('Plus')}
+      />
+      <Player
+        name="Jane Doe"
+        score="30"
+        onMinus={() => console.log('Minus')}
+        onPlus={() => console.log('Plus')}
+      />
+    </>
   )
 }
 
-export default App;
+export default App
