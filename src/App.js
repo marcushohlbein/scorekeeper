029529c9ced815ac7d6474b0f001cbc28e959f2d
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-23 08:20:10 +0100

    Add app logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import PlayerForm from './components/PlayerForm'
 import Player from './components/Player'
 import Button from './components/Button'
 
 export default function App() {
+  const [players, setPlayers] = useState([])
+
+  function handleAddPlayer(name) {
+    setPlayers(oldPlayers => [...oldPlayers, { name, score: 0 }])
+  }
+
+  function resetAll() {
+    setPlayers([])
+  }
+
+  function resetScore() {
+    setPlayers(players.map(player => ({ ...player, score: 0 })))
+  }
+
+  function handlePlus(index) {
+    const currentPlayer = players[index]
+    setPlayers([
+      ...players.slice(0, index),
+      { ...currentPlayer, score: currentPlayer.score + 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function handleMinus(index) {
+    const currentPlayer = players[index]
+    setPlayers([
+      ...players.slice(0, index),
+      { ...currentPlayer, score: currentPlayer.score - 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
   return (
     <main className="App">
       <PlayerForm label="Add Player:" onAddPlayer={name => console.log(name)} />
