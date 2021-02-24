988f9aeea8d81466352755aa58fdca43cb903a7d
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-24 10:28:23 +0100

    Small fixings

--- src/components/PlayerItem.js
+++ src/components/PlayerItem.js
@@ -4,7 +4,7 @@ export default function PlayerItem({ name, score, onMinus, onPlus }) {
   return (
     <Player>
       {name}
-      <PlayerScore>
+      <PlayerScore score={Math.min(140, Math.max(0, score * 7))}>
         <button onClick={onMinus}>-</button>
         <span>{score}</span>
         <button onClick={onPlus}>+</button>
@@ -24,4 +24,16 @@ const PlayerScore = styled.div`
   gap: 5px;
   grid-template-columns: repeat(3, 1fr);
   place-items: center;
+
+  span {
+    color: ${props => 'hsl(' + props.score + ', 70%, 60%);'};
+  }
+
+  button {
+    border: none;
+    border-radius: 10px;
+    background: #bbb;
+    width: 35px;
+    height: 35px;
+  }
 `
