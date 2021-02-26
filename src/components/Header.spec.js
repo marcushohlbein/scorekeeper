79d361f67ea513d9939b9831e8a5c510c31b62f3
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-26 19:16:20 +0100

    Fix header test

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react'
 import Header from './Header'
 
 describe('Header', () => {
-  it('return a div with a title', () => {
+  it('returns a header with a title', () => {
     render(<Header text="Title" />)
     expect(screen.getByRole('banner')).toBeInTheDocument()
+    expect(screen.getByRole('banner').textContent).toBe('Title')
   })
 })
